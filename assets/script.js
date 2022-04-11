6c95c92f3909351ba82e392ad35b516395637264
Ricky Thakar <rickythakar@gmail.com>
2022-04-10 22:47:50 -0400

    initial commit. Created an index file and hooked it up to js file

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+console.log("js file is hooked up")
+//Global Variables
+const searchHistory = [];
+const weatherApiUrl = "https://api.openweathermap.org/";
+
+
+//Dom Element References 
+// #search form. #search-input, #today, #forecast, #history
+
+let searchForm = document.querySelector("#search-form");
+let searchInput 
+let todayBox
+let forecastBox
+let searchDisplayBox
+
+// Add timezone for any 
+
+
+// Fuinction to display search history 
