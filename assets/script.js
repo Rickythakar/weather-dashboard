03d8c6ce7e6ac5061f2925032d3562f6e4eadce7
Ricky Thakar <rickythakar@gmail.com>
2022-04-11 20:02:05 -0400

    created wireframe of project and added bordsers to containers

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,21 @@ console.log("js file is hooked up")
 //Global Variables
 const searchHistory = [];
 const weatherApiUrl = "https://api.openweathermap.org/";
-const apiKey = "88a754472e95b886547fd49c550f9fe6";
 const apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=88a754472e95b886547fd49c550f9fe6";
+          // This calls the API, just update the url to have your key's name.
+          async function fetchKey() {
+            const url = 'https://yorkieportunus.herokuapp.com/store/weather-dash'
+            const response = await fetch(url);
+            const key = await response.json();
+            return key;
+        }
+        // Call this wherever you need your key.
+        fetchKey().then((key) => {
+            secretKey = key.apiKey;
+            console.log(secretKey);
+        });
+      
+    
 console.log(apiUrl);
 
 
@@ -20,8 +33,29 @@ let searchDisplayBox
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
 
-// Fuinction to display search history 
+function displaySearchHistory() {
+    var apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=88a754472e95b886547fd49c550f9fe6`;
+    fetch(apiUrl)
+    .then(function(response){
+    return response.json()})
+    console.log(apiUrl)
+}
+    
+
+// Function to display search history 
+// Function to update history and local storage
+// Function to grab history from local storage
+// Function to display current weather data fetch from openweather api
+// Function to display a forecast card
+// Function to display five day forecast
+// Function to render city and data
+// Function to grab weather data location using geolocation
+// Function to fetch coordinates
+// Function to handle search forms
+// Function to handle click history
 // Function to displaySearchHistory
+// Call back function to grab search history from local storage
+// Two add event listners for the search history and search submission
 //  searchDisplayBox.innerHTML = "";
 
 // create a fetch request for api key
@@ -31,7 +65,6 @@ dayjs.extend(window.dayjs_plugin_timezone);
 //         response.json()
 //     })
 //     .then(function(data)
-
 //     })
 // }
 
