e1cece4d28553f39a4ebd7e5284f86208d0e75f3
Ricky Thakar <rickythakar@gmail.com>
2022-05-31 12:34:33 -0400

    moved api key from key file into script file

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
-console.log("js file is hooked up")
 //Global Variables
 const searchHistory = [];
 const weatherApiUrl = "https://api.openweathermap.org/";
 const apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=88a754472e95b886547fd49c550f9fe6&units=imperial";
+const apiKey = "88a754472e95b886547fd49c550f9fe6";
 // This calls the API, just update the url to have your key's name.
 async function fetchKey() {
   const url = 'https://yorkieportunus.herokuapp.com/store/weather-dash'
